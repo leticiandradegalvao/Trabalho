647df14501ca0ef260b57142ceddac9ab9cb9dfc
leticiandradegalvao <leticiaandrade.galvao@estudante.ufjf.br>
2025-11-25 21:18:43 -0300

    inicializaçao

---
+++ jogodasenha.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <random>
+using namespace std;
+
+int main (){
+    int inicializacao;
+
+  cout << "JOGO DA SENHA" << endl;
+  cout << "\nSeja bem vindo!";
+  cout << "\nVoce tem 10 tentativas para acertar a senha de 4 digitos";
+  cout << "\nDigite 0 para iniciar";
+
+  cin >> inicializacao;
+  
+if (inicializacao)
+  return 0;
+  
+}
