a0ffc48cc48aa7659c66df980b5a29157271f91e
leticiandradegalvao <leticiaandrade.galvao@estudante.ufjf.br>
2025-11-29 14:16:06 -0300

    função para validar o palpite

--- jogodasenha.cpp
+++ jogodasenha.cpp
@@ -2,6 +2,7 @@
 #include <random>
 using namespace std;
 
+
 //função que cria a senha//
 int criarSenha() {
     random_device rd;
@@ -16,15 +17,41 @@ int criarSenha() {
     return senha;
 }
 
+
 //quebrar senha termo por termo//
-//jogador digitou 1 a 6 e se tem 4 dígitos//
+
+
+//validar se tem 4 digitos e se são de 1-6//
+void validarPalpite (string palpite) {
+
+  if (palpite.length() != 4) { //4 algarismos//
+    cout << "Senha inválida (deve ter somente 4 dígitos)\n";
+    return;
+  }
+  for (int i = 0; i < 4; i++) {
+    string digito = palpite.substr(i, 1); //separar digitos na string//
+
+    if (digito < "0" || digito > "9") {
+      cout << "Valor inválido (deve ter somente números)\n"; //se o palpite tiver letras ou outros caracteres//
+      return;
+    }
+
+    if (digito < "1" || digito > "6") { //validar 1-6//
+      cout << "O " << i + 1 << " ° algarismo é inválido, digite outro valor.\n";
+      return;
+    }
+  }
+  }
+
+
 //fazer funcao para verificar tentativas//
 
 
 int main (){
   //armazenar senha//
     int senha = criarSenha();
-    int palpite; 
+
+    string palpite;
 
   cout << "JOGO DA SENHA" << endl;
   cout << "\nSeja bem vindo!";
@@ -32,9 +59,10 @@ int main (){
   cout << "Digite a senha: ";
   cin >> palpite;
 
+  //4 dígitos e 1-6//
+  validarPalpite(palpite); 
   
   //chamar quebrar a senha//
-  //jogador digitou 1 a 6 e se tem 4 dígitos//
   //chamar funcao verificar tentativas//
 
   return 0;
